79267a01e41cbe74d330078f18578ebe4c3d069e
finkenauer <pfinkenauer@inf.ufpel.edu.br>
2016-09-15 21:10:21 -0300

    arquitetura do rippleCarry implementada

--- Adders/RippleCarry/rippleCarry.vhd
+++ Adders/RippleCarry/rippleCarry.vhd
@@ -6,14 +6,14 @@ use ieee.std_logic_1164.all;
 entity rippleCarry is
 
 generic (
-	N 	: integer);
+	N 	: integer := 32);
 
 port (
 	x 	 : in std_logic_vector((N-1) downto 0);
 	y 	 : in std_logic_vector((N-1) downto 0);
 	cin : in std_logic;
 	s   : out std_logic_vector((N-1) downto 0);
-	cout: out std_logic
+	cout: out std_logic_vector((N-1) downto 0)
 );
 end rippleCarry;
 
@@ -28,12 +28,12 @@ port(
 	s	 : out std_logic;
 	cout: out std_logic
 );
-end component
+end component;
 
 begin
-adder32: fullAdder
-	N => 32
-	GEN_REG:
+	GEN_ADD:
 	for  i in 0 to N-1 generate
-
+		adder32: fullAdder port map
+			(a=>x(i), b=>y(i), cin=>cin, s=>s(i), cout=>cout(i));
+	end generate GEN_ADD;
 end behv_rippleCarry;
