5e2350e57f9d9ef3b99c064a73098f5406aaa30f
finkenauer <pfinkenauer@inf.ufpel.edu.br>
2016-10-12 22:27:31 -0300

    clock adicionado

--- Adders/RippleCarry/rippleCarry.vhd
+++ Adders/RippleCarry/rippleCarry.vhd
@@ -9,6 +9,7 @@ generic (
 	N 	: integer := 32);
 
 port (
+	clk : in std_logic;
 	x 	 : in std_logic_vector((N-1) downto 0);
 	y 	 : in std_logic_vector((N-1) downto 0);
 	cin : in std_logic;
@@ -31,17 +32,29 @@ port(
 );
 end component;
 
+	signal xtemp, ytemp, stemp: std_logic_vector((N-1) downto 0);
+
 begin
+
+process(clk)
+	begin	
+			if(rising_edge(clk)) then
+				s <= stemp;
+				xtemp <= x;
+				ytemp <= y;
+			end if;
+	end process;
+
 	adder0: fullAdder port map
-			(a=>x(0), b=>y(0), cin=>cin, s=>s(0), cout=>c(1));
+			(a=>xtemp(0), b=>ytemp(0), cin=>cin, s=>stemp(0), cout=>c(1));
 	
 	GEN_ADD:
 	for  i in 1 to N-2 generate
 		adderI: fullAdder port map
-			(a=>x(i), b=>y(i), cin=>c(i), s=>s(i), cout=>c(i+1));
+			(a=>xtemp(i), b=>ytemp(i), cin=>c(i), s=>stemp(i), cout=>c(i+1));
 	end generate GEN_ADD;
 	
 	adder31: fullAdder port map
-			(a=>x(31), b=>y(31), cin=>c(31), s=>s(31), cout=>cout);
+			(a=>xtemp(N-1), b=>ytemp(N-1), cin=>c(N-1), s=>stemp(N-1), cout=>cout);
 		
 end behv_rippleCarry;
